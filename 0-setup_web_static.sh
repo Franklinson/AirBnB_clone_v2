57431c3841b4a8ac7c12d46bd45beead47d06d5f
Franklinson <franklinhassey@gmail.com>
2023-09-07 17:16:36 +0000

    setup a static web

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,28 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployment of web_static
 
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
-sudo chown -R ubuntu:ubuntu /data/
-
-echo "<html>
-  <head></head>
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
   <body>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-if [ -L /data/web_static/current ]; then
-    sudo rm /data/web_static/current
-fi
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-nginx_config="/etc/nginx/sites-available/default"
-nginx_alias="location /hbnb_static/ {\n    alias /data/web_static/current/;\n}\n"
+sudo chown -R ubuntu:ubuntu /data/
 
-if ! grep -q "location /hbnb_static/" "$nginx_config"; then
-    sudo sed -i "/server_name _;/a $nginx_alias" "$nginx_config"
-fi
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
