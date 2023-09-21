74aa52ab6a8dea4f63104b1b0bf919b45c439f1c
Franklinson <franklinhassey@gmail.com>
2023-09-21 13:42:25 +0000

    set default variables for route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Adding a route to the previous codes to accept variables
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Route definition with strict_slashes=False
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+# Second page for
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return "HBNB"
+
+
+# Third page for web app
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    formatted_text = text.replace('_', ' ')
+    return f"C {formatted_text}"
+
+
+# Set a default variable for text
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+if __name__ == '__main__':
+    # Run the Flask app on 0.0.0.0 and port 5000
+    app.run(host='0.0.0.0', port=5000)
